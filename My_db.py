60e9a223be7926d1277bfebbaf603a6f84a7651e
BrennonL <104086501+BrennonL@users.noreply.github.com>
2024-06-05 09:45:10 -0600

    My_db.py comments

--- My_db.py
+++ My_db.py
@@ -15,17 +15,8 @@ class My_db:
     def get_csv(self) -> pd.DataFrame:
         '''
         :fun: Converts the pre-existing dataset in csv file (corresponding with the year)
-            and converts it to a pandas Dataframe.
+            and converts it to a pandas DataFrame. It then returns this DataFrame.
         '''
-        #! Self reading CSV
-        # if os.path.exists(self.path):
-        #     my_df = pd.DataFrame(data=None, columns=["Date", "Type", "Amount", "Note"])
-        #     with open(self.path, "r") as year_file:
-        #         for line in year_file:
-        #             data = line.split(', ')
-        #             new_row = {"Data" : data[0], "Type": data[1], "Amount": data[2], "Note": data[3]}
-        #             my_df = my_df.append(new_row, ignore_index = True)
-        #! With the Pandas reading CSV
         if os.path.exists(self.path):
             my_df = pd.read_csv(self.path)
         else:
@@ -34,6 +25,11 @@ class My_db:
     
 
     def display_month(self, month):
+        '''
+        :arg month: int (should be 2 integers representing a month)
+        :fun: Uses a regex and df.Date.str.match() function to query the months
+            that are passed into the function from the df.
+        '''
         regex = f"{self.year}-{month}-[0-9][0-9]"
         print(self.df[self.df.Date.str.match(regex)])
 
@@ -48,13 +44,29 @@ class My_db:
         ...
     
     def add_row(self, tran_type, amount, note):
+        '''
+        :arg tran_type: str (transaction type)
+        :arg amount: int
+        :arg note: str
+        :fun: Takes the following input and creates a new row in the self.df
+            using the df.append() function.
+        '''
         new_row = {"Date" : self.date, "Type": tran_type, "Amount": amount, "Note": note}
         #TODO -- Change this to Concat instead of append
         self.df = self.df.append(new_row, ignore_index = True)
     
     def delete_row(self, this_index):
+        '''
+        :arg this_index: int
+        :fun: Uses the parameter this_index to delete a row at the specified 
+            index on the self.df. For example it 0 was passed in, the first row
+            on the self.df would be deleted.
+        '''
         self.df = self.df.drop(self.df.index[this_index])
 
     def put_back_csv(self):
-        ''' This will put the df back into the csv file'''
+        '''
+        :fun: Save the self.df as a CSV file in the files folder. If the folder
+            doesn't exist it should create it.
+        '''
         self.df.to_csv( f".\\files\\{self.year}.csv", sep=",", index_label=None, index=None )
